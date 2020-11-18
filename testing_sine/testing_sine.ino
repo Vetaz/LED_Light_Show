899388bf13dbbd8c2ee195412daafacee0164f85
Vetaz <47232579+Vetaz@users.noreply.github.com>
2020-11-18 12:41:56 -0700

    Added Sam's code with some minor changes

--- testing_sine/testing_sine.ino
+++ testing_sine/testing_sine.ino
@@ -1,97 +1,63 @@
-
-// Sample Rate = 8000, sineWave has 32 samples which is 1 full wave 
-// 1 period = SamplesInWave / SampleRate = 0.004 seconds
-// Frequency = 1 / Period = 250 Hz
-// Tone generated at 250Hz with an amplitude of .5.
-static float sineWave[32] = {0.00000,0.09755,0.19134,0.27779,0.35355,0.41573,0.46194,0.49039,0.50000,0.49039,0.46194,0.41573,0.35355,0.27779,0.19134,0.09755,0.00000,-0.09755,-0.19134,-0.27779,-0.35355,-0.41573,-0.46194,-0.49039,-0.50000,-0.49039,-0.46194,-0.41573,-0.35355,-0.27779,-0.19134,-0.09755};
-int sineWaveSize;
-static int sampleRate = 8000;
-
-static uint16_t frequencies[10] = {60, 90, 120, 240, 480, 960, 1250, 2500, 5000, 10000};
-static uint8_t currentFreqNum = 0;
-
-static uint8_t brightnesses[18] = {130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 210, 200, 190, 180, 170, 160, 150, 140};
-static uint8_t currentBrightnessNum = 0;
-
-static uint8_t currentLEDs = 0;
-
-//TODO: volume changes num leds on...
-// Sine wave: y = A sin (k (theta - b)) + c
-// A = Amplitude, k = periodicity factor, b = left/right shift, c = up/down shift
-
-// 60-250 HZ = base
-
 #include <FastLED.h>
 #include <math.h>
 
-// Using 60 Hz - 10 kHz for range of colors
-// hue = Log2(freq) - Log2(LOWER_FREQ) / (Log2(UPPER_FREQ) - Log2(LOWER_FREQ)) * 359
-#define UPPER_FREQ  10000
-#define LOWER_FREQ  60
-
 #define LED_PIN     6
-#define NUM_LEDS    60 // Change to 300 for Sam's LED Strip; 60 for Jordi's.
-#define BRIGHTNESS  32 // It can be higher, but keep at 32 for now?
+#define TOTAL_NUM_LEDS    60 // Change to 300 for Sam's LED Strip; 60 for Jordi's.
 #define LED_TYPE    WS2812
 #define COLOR_ORDER GRB
 #define UPDATES_PER_SECOND 100 // how fast the LEDs change color/brightness per second.
+int analogPin = A3; // potentiometer wiper (middle terminal) connected to analog pin 3
+                    // outside leads to ground and +5V
+int analogVal = 0;  // variable to store the value read
+uint8_t brightness = 32; //at 32 just to start out
 
-CRGB leds[NUM_LEDS];
+CRGB leds[TOTAL_NUM_LEDS];
 
-void setup() { 
+void setup() {
   delay(3000); // power-up safety delay
   Serial.begin(9600);
-  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS); 
-  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, TOTAL_NUM_LEDS);
+  FastLED.setBrightness(brightness);
 
-  sineWaveSize = sizeof(sineWave) / sizeof(float);
 }
 
-void FillLEDs(uint8_t hue, uint8_t brightness, uint8_t saturation)
+void FillLEDs(uint8_t hue, uint8_t brightness, uint8_t saturation, uint8_t numLEDs)
 {
   // Brightness from 0-255;
   // Saturation from 0-255;
-  currentLEDs++;
-  // Turn on a fraction of the LEDs if birghtness is not turned all the way up
-  uint8_t adjNumLEDs = brightness / 255 * NUM_LEDS; 
-  for(int i = 0; i < currentLEDs % NUM_LEDS; i++) {
+  // Checks on brightness and Saturation that they are in range.
+  if (brightness < 0 || brightness > 255) {
+    if (brightness < 0) {
+      brightness = 0;
+    } else {
+      brightness = 255;
+    }
+  }
+  if (saturation < 0 || saturation > 255) {
+    if (saturation < 0) {
+      saturation = 0;
+    } else {
+      saturation = 255;
+    }
+  }
+  
+  for(int i = 0; i < numLEDs; i++) {
     leds[i] = CHSV(hue, saturation, brightness);
   }
-  for(int i = currentLEDs % NUM_LEDS; i < NUM_LEDS; i++) {
-    leds[i] = CHSV(hue, saturation, 0);
-  }
 }
 
-/* Get the pitch frequency */
-float getFreq() {
-  currentFreqNum++;
-  return frequencies[currentFreqNum % 11]; // Going through the frequencies each time this function is called.
-}
-
-/* Get the volume amplitude 
-   returns a number from 0 to 255 for brightness */
-uint8_t getBright(){
-//  currentBrightnessNum++;
-//  return brightnesses[currentBrightnessNum % 19]; // Going through the brightnesses each time this function is called.
-
-  return (volt - 3.3) / (5 - 3.3) * 255;
-}
-
-void loop() { 
-  static uint8_t hue = 0;
+void loop() {
+  uint8_t hue = 0;
   static uint8_t saturation = 255;
-  
-  static uint8_t startIndex = 0;
-  startIndex = startIndex + 1; /* motion speed */
 
-  float freq = getFreq();
-  uint8_t brightness = getBright();
+  analogVal = analogRead(analogPin);  // read the input pin, returns 0-1023 based on voltage 0v-5v
 
-  // Changes color based on Frequency.
-  hue = ((log(freq)/log(2)) - (log(LOWER_FREQ)/log(2))) / ((log(UPPER_FREQ)/log(2)) - (log(LOWER_FREQ)/log(2))) * 250; // For 60 Hz, Hue = 0; for 10 kHz, Hue = 250
-  
-  FillLEDs(hue, brightness, saturation);
-  
+  brightness = (uint8_t)(((float) analogVal / 1023) * 255); //brighter the higher the voltage, casting to float to perform math
+  hue = (uint8_t)(((float) analogVal / 1023) * 255); //different hue with differet voltage
+  uint8_t numLEDs = (uint8_t)(((float) analogVal / 1023) * TOTAL_NUM_LEDS); //the number of LEDS that should be on based on analog input
+ 
+  FillLEDs(hue, brightness, saturation, numLEDs);
+ 
   FastLED.show();
   delay(1000 / UPDATES_PER_SECOND);
 }
