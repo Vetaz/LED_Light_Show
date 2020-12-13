b4317cf4a23c62656fa3269a59eef5189aff7087
Vetaz <47232579+Vetaz@users.noreply.github.com>
2020-12-12 17:04:33 -0700

    Cleaned up code

--- led_vis/led_vis.ino
+++ led_vis/led_vis.ino
@@ -1,43 +1,38 @@
 #include <FastLED.h>
 #include <math.h>
 
-#define LED_PIN             6
-#define TOTAL_NUM_LEDS      253 // 253 for Sam's LED Strip; 60 for Jordi's.
+#define LED_PIN             6       // Data pin for the LED strip
+#define TOTAL_NUM_LEDS      300     // Change to how many LEDs you want to show
 #define LED_TYPE            WS2812
 #define COLOR_ORDER         GRB
-#define UPDATES_PER_SECOND  100 // how fast the LEDs change color/brightness per second.
+#define UPDATES_PER_SECOND  100     // How fast the LEDs change color/brightness per second.
+#define HIGHEST_VOLTAGE     746     // The cut-off voltage from 0-1023. 
+                                    // The lower the number, the more LEDs are lit for a specific voltage.
 
-int analogPin = A3; // potentiometer wiper (middle terminal) connected to analog pin 3
+int analogPin = A3; // Potentiometer wiper (middle terminal) connected to analog pin 3
                     // outside leads to ground and +5V
-int analogVal = 0;  // variable to store the value read
-uint8_t overallBrightness = 100; // a 0-255 value for how much to scale all leds before writing them out
+int analogVal = 0;  // A variable to store the value read
+uint8_t overallBrightness = 100; // A 0-255 value for how much to scale all leds before writing them out
 uint8_t prevNumLEDs = 0;
+
 
 CRGB leds[TOTAL_NUM_LEDS];
 
 void setup() {
-  delay(3000); // Power-up safety delay
+  delay(3000); // 3 second power-up safety delay
   FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, TOTAL_NUM_LEDS);
   FastLED.setBrightness(overallBrightness);
   Serial.begin(9600);
 }
 
 /* Fill LEDs up to the numLEDs */ 
-void fillLEDs(uint8_t saturation, uint8_t numLEDs)
+void fillLEDs(uint8_t numLEDs)
 {
   uint8_t hue;
   uint8_t brightness;
   
-  // Making sure saturation is 0-255
-  if (saturation < 0 || saturation > 255) {
-    if (saturation < 0) {
-      saturation = 0;
-    } else {
-      saturation = 255;
-    }
-  }
-    // Making sure numLEDs is 0-253 (if voltage reads over 746, fill all LEDs)
-  if (numLEDs < 0 || numLEDs > 253) {
+  // Making sure numLEDs is 0-TOTAL_NUM_LEDS (if voltage reads over 746, fill all LEDs)
+  if (numLEDs < 0 || numLEDs > TOTAL_NUM_LEDS) {
     if (numLEDs < 0) {
       numLEDs = 0;
     } else {
@@ -46,9 +41,9 @@ void fillLEDs(uint8_t saturation, uint8_t numLEDs)
   }
  
   for(int i = 0; i < numLEDs; i++) {
-    hue = (uint8_t) ((float)i / TOTAL_NUM_LEDS * 250); // going up to hue value of 250 instead of the max of 255.
+    hue = (uint8_t) ((float)i / TOTAL_NUM_LEDS * 250); // LED colors go from Red (0) to Pink (250)
     brightness = (uint8_t) ((float)i / numLEDs * 55 + 200); // Brightness from 200 to 255.
-    leds[i] = CHSV(hue, saturation, brightness);
+    leds[i] = CHSV(hue, 255, brightness);
     if (numLEDs < 48 && i % 6 == 0){ // Show filled LEDs every 6 LEDS
         FastLED.show();
     }
@@ -65,41 +60,24 @@ void fillLEDs(uint8_t saturation, uint8_t numLEDs)
 void unfillLEDs(uint8_t prevNumLEDs, uint8_t numLEDs) { 
   for (int i = prevNumLEDs; i >= numLEDs; i--)
   {
-    leds[i] = CRGB(0, 0, 0);
-//    if (prevNumLEDs > 96 && i % 12 == 0){ // unfill every 6 LEDS
-//        FastLED.show();
-//    }
-//    else 
-    if (prevNumLEDs > 96 && i % 8 == 0){ // unfill every 8 LEDS
+    leds[i] = CRGB(0, 0, 0); // Turn off LED
+    if (prevNumLEDs > 96 && i % 8 == 0){ // Unfill every 8 LEDS
         FastLED.show();
     }
-    else if (i % 12 == 0){ // unfill every 12 LEDS
+    else if (i % 12 == 0){ // Unfill every 12 LEDS
         FastLED.show();
     }
   }
 }
 
-// code to help test for Jordi
-//  static int digitalInput[1] = {1023}; //{100,200,300,450,300,100,500,700,800,1023,1023,800,300,200,100,400,500,700,800,500,400,600,300};
-//  uint8_t index = 0;
-// end of Jordi's code to help test.
-
 void loop() {
-  static uint8_t saturation = 255;
- 
-  analogVal = analogRead(analogPin);  // read the input pin, returns 0-1023 based on voltage 0v-5v
+  analogVal = analogRead(analogPin);  // Read the input pin, returns 0-1023 based on voltage 0v-5v
 
-  // code to help test for Jordi commented out for you, Sam.
-//  analogVal = digitalInput[index % 1];
-//  index++;
-  // end of Jordi's code to help test.
-
-  // brightness =(uint8_t)(((float) analogVal / 1023) * 255); //brighter the higher the voltage, casting to float to perform math
-  // hue = (uint8_t)(((float) analogVal / 1023) * 255); //different hue with differet voltage
-  uint8_t numLEDs = (uint8_t)(((float) analogVal / 746) * TOTAL_NUM_LEDS); //the number of LEDS that should be on based on analog input
+  // The number of LEDS that should be on based on analog input
+  uint8_t numLEDs = (uint8_t)(((float) analogVal / HIGHEST_VOLTAGE) * TOTAL_NUM_LEDS); 
 
   if (prevNumLEDs < numLEDs) {
-    fillLEDs(saturation, numLEDs);
+    fillLEDs(numLEDs);
   } else {
     unfillLEDs(prevNumLEDs, numLEDs);
   }
